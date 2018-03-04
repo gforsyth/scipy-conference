1e879f47f49fb7c42a58cb280301dc2e351ae610
Gil Forsyth <gilforsyth@gmail.com>
2018-03-04 17:03:11 -0500

    Add bookmarklet generator to hide non-domain papers
    Because Easychair is so incredibly stupid, here is a script that returns
    a string which can be used as a javascript bookmarklet and will hide all
    papers not in a given domain

---
+++ 2017/review_bookmarklet_generator.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from ldassn import load_rev_sublist
+
+from functools import lru_cache
+
+
+def generate_js_bookmarklet(html_doc, domain):
+    """
+    Generate the javascript for a bookmarklet that hides papers not in domain `domain`
+
+    Arguments
+    ---------
+    html_doc : str
+        source of the table in easychair status page (very long string)
+    domain : str
+        name of domain you want to view (i.e. filter all other domains)
+    """
+    # load submissions
+    _, sublist, _, _ = load_rev_sublist()
+
+    subdf = get_papers_df(html_doc, sublist)
+
+    jsdomain = subdf[subdf.domain != domain][0].values
+    jsdomain = [dom.strip().replace('l','r') for dom in jsdomain]
+
+    js = r"javascript:(function(){var%20papers=" + f"{jsdomain}" + r";for(paper%20in%20papers){var%20d=document.getElementById(papers[paper]);if(d.hidden==true){d.hidden=false;}else{d.hidden=true}};})()"
+
+    return js
+
+@lru_cache(maxsize=None)
+def get_papers_df(html_doc, sublist):
+    """
+    Parse html_doc, grab the list of element ids of all papers, then 
+    merge them into a dataframe along with the domain information
+    """
+    # create long raw string variable of source from status page in easychair
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    papers = []
+    for item in soup.find_all('a'):
+        if item.get('class'):
+            if item['class'][0] == 'title':
+                papers.append((item['id'], item.text))
+
+    subdf = pd.DataFrame(papers)
+    domain = []
+
+    # iterate over papers df and create 'domain' column
+    for i, id, title in subdf.itertuples():
+        res = [sub.domain for sub in sublist if sub.title == title.strip()]
+        if res:
+            domain.append(res[0])
+        else:
+            domain.append('scipy tools')
+            
+    subdf = subdf.assign(domain=domain)
+    return subdf
+                
